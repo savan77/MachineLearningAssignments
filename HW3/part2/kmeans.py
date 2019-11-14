4921a602c3513bf271411b6e2d142baf5abcda1e
savan77 <vsavan7@gmail.com>
2019-11-13 18:36:14 -0600

    update centroids

--- HW3/part2/kmeans.py
+++ HW3/part2/kmeans.py
@@ -18,31 +18,54 @@ class KMeans:
 		# infinite loop might not be a good idea so instead of stopping training after convergence
 		# we will loop for some predefined epochs and if our model converes before `epochs` we will terminate training then
 		for epoch in range(epochs):
-			clusters = {i:[] for i in range(self.K)}
+			print("#############################################")
+			print("Epoch: ", epoch)
+			print("Centroid: ", self.centroids)
+			self.clusters = {i:[] for i in range(self.K)}
 			for text in self.data:
+				
 				#calculate distance form each centroid
 				distances = [self.jaccard(text, self.centroids[cent]) for cent in self.centroids]
 				#find the centroid with min distance
 				min_distance = distances.index(min(distances))
-				clusters[min_distance].append(text)
+				self.clusters[min_distance].append(text)
 
 			# find new cluster
-			prev_centroids = self.centroids
-			for cluster in clusters:
-				self.centroids[cluster] = self.find_min(clusters[cluster])
 
+			prev_centroids = self.centroids.copy()
+			for cluster in self.clusters:
+				# print(cluster)
+				self.centroids[cluster] = self.find_min(self.clusters[cluster])
+			print("New", self.centroids)
 			converged = True
 			for cent in self.centroids:
+				# print("cent", cent)
+				# print(self.jaccard(prev_centroids[cent], self.centroids[cent]))
 				if self.jaccard(prev_centroids[cent], self.centroids[cent]) > 0.001:
+					# print("conv", converged)
 					converged = False
-
+					break
+			print(self.sse())
 			if converged:
 				break
 
+		print("----------------------------------------")
+		print("K: ", self.K)
+		print("SSE: ", self.sse())
+		print([len(v) for c,v in self.clusters.items()])
 
 
-	def find_min(self):
-		pass
+
+	def find_min(self, clusters):
+		distances = []
+		dists_point = []
+		for idx, point in enumerate(clusters):
+			temp = 0
+			for j in clusters[idx+1:]:
+				temp += self.jaccard(point, j)
+			distances.append(temp)
+			dists_point.append(point)
+		return dists_point[distances.index(min(distances))]
 
 	def jaccard(self, a, b):
 		"""
@@ -56,15 +79,15 @@ class KMeans:
 		sse_total = 0
 		for i in range(self.K):
 			for j in range(len(self.clusters[i])):
-				sse_total += self.jaccard(self.clusters[i][j]['centroid'], self.clusters[i][j])
+				sse_total += self.jaccard(self.centroids[i], self.clusters[i][j])
 		return sse_total
 
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--K",  default=5, help="number of clusters")
+	parser.add_argument("--K",  default=23, help="number of clusters")
 	parser.add_argument("--data", default="processed_data.csv")
 	parser = parser.parse_args()
 	km = KMeans(parser.K, parser.data)
-	km.fit(3)
+	km.fit(30)
